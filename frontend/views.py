8806f97104540bb2c06035225f04535e4815f43a
Setasena Randata Ramadanie <setasena.randata@ui.ac.id>
2022-05-10 02:22:56 +0700

    Trying to connect to database

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,11 @@
+from cgi import test
 from django.shortcuts import render
+from django.db import connection
 
 def home(request):
+    cursor = connection.cursor()
+    cursor.execute("select * from auth_user;")
+    test_manggil = cursor.fetchall()
+    print(test_manggil)
     return render(request, 'home.html')
 # Create your views here.
